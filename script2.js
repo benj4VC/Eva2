0252ae7868a17335f55a9e691311482789ba631e
benj4VC <b.e.n.j.a.250806@gmail.com>
2025-05-26 14:38:17 -0400

    commit final

--- script2.js
+++ script2.js
@@ -1,20 +1,18 @@
 //validar nombre, debe contener solo letras y no ser vacio
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
 let personas =[]
-
 function validar(){
     let eNombre = document.getElementById("nombre")
     let vNombre = eNombre.value
     let eErrorNombre = document.getElementById("errorNombre")
-    let fNombre = validarLargoMinimo(eNombre,vNombre,eErrorNombre)
          let p = {
             nombre:vNombre,
         }
-        personas.push(p)
         console.log(personas)
         eNombre.value = ""
         cargarDatos()
     }
+ 
 
 
 
